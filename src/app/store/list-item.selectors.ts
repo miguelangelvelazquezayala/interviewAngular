8b8fee76567cc9c9af12d51e495e0ca315ac5078
ChipiSyncfy <chipi@paybook.me>
2023-03-27 17:01:11 -0600

    [fix] Fix store

--- src/app/store/list-item.selectors.ts
+++ src/app/store/list-item.selectors.ts
@@ -1,7 +1,7 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { State } from './list-item.reducer';
 
-export const getListItemsState = createFeatureSelector<State>('list-item');
+export const getListItemsState = createFeatureSelector<State>('listItem');
 
 export const getList = createSelector(
   getListItemsState,
